35f875260f46a85c52ab73211746204df343577c
Jay Ma <jayma424@gmail.com>
2012-01-13 10:28:04 -0800

    First Commit

---
+++ src/org/jay/CarLog/CarLogActivity.java
@@ -0,0 +1,30 @@
+package org.jay.CarLog;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CarLogActivity extends Activity 
+{    
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button next = (Button)findViewById(R.id.button1);
+        next.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View view) 
+            {
+                Intent myIntent = new Intent(view.getContext(), ManageVehiclesActivity.class);
+                startActivityForResult(myIntent, 10);
+            }
+
+        });
+        
+    }
+}
---
+++ src/org/jay/CarLog/ManageVehiclesActivity.java
@@ -0,0 +1,16 @@
+package org.jay.CarLog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ManageVehiclesActivity extends Activity
+{
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+    	 super.onCreate(savedInstanceState);
+         setContentView(R.layout.manage_vehicles);
+    }
+}
